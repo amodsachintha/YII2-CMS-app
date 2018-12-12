b1a9371a4a6a78a2bde5649774c9ba8256a6858b
amodsachintha <amod077@gmail.com>
2018-12-12 10:02:05 +0530

    init

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=chat_cms',
+    'username' => 'root',
+    'password' => 'toor',
+    'charset' => 'utf8mb4',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
